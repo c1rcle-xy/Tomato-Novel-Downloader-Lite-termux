4fd9e2a53602a0216b9e570ad6f1e2bce69d6f1a
c1rcle <100463851+c1rcle-xy@users.noreply.github.com>
2025-03-13 13:05:22 +0800

    Create global_install.sh

---
+++ global_install.sh
@@ -0,0 +1,17 @@
+termux-setup-storage
+apt update && apt upgrade
+#pip config set global.index-url https://mirrors.aliyun.com/pypi/simple
+pkg install python libxml2 libxslt pkg-config openssl -y
+#pip config set global.index-url https://mirrors.aliyun.com/pypi/simple/
+pip install cython wheel
+CFLAGS="-Wno-error=incompatible-function-pointer-types -O0" pip install lxml
+#install lxml
+pip install requests lxml tqdm beautifulsoup4
+echo "Download from the GitHub. If you cannot download it, you can use the install.sh script instead."
+curl -o Tomato-Novel-Downloader-Lite.zip "https://github.com/Dlmily/Tomato-Novel-Downloader-Lite/archive/refs/heads/main.zip" || { echo "error,something went wrong"; exit 1; }
+unzip -o -d ~/storage/downloads Tomato-Novel-Downloader-Lite.zip
+#unzip
+cd ~/storage/downloads/Tomato-Novel-Downloader-Lite-main
+sed -i '$ a\alias fq="cd ~/storage/downloads/Tomato-Novel-Downloader-Lite-main && python ./2.py"' ~/.bashrc
+#environment variable
+python 2.py
